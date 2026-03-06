0ef45e5c0e70b74c4815ea7fd691ae526cdd5e5e
mateo <munozomateo@gmail.com>
2026-03-05 22:47:08 -0500

    creación schema user

---
+++ src/schemas/user_schema.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+from pydantic.v1 import EmailStr
+from uuid import UUID
+
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone: str | None = None
+    address: str | None = None
+
+class UserCreate(UserBase):
+    password: str
+
+class UserUpdate(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    email: EmailStr | None = None
+    password: str | None = None
+    phone: str | None = None
+    address: str | None = None
+
+class UserResponse(UserBase):
+    id: UUID
+    date_created: datetime | None = None
+    date_updated: datetime | None = None
+
+    class Config:
+        from_attributes = True
