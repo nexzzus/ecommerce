b6a0a089e08bd71368f2c4da4f7238fa73fb293a
mateo <munozomateo@gmail.com>
2026-03-27 20:10:50 -0500

    ci_push.yml corregido

--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,10 @@
 import os
 import sys
 from logging.config import fileConfig
-from sqlalchemy import engine_from_config, pool
+
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
 
 # 1. CONFIGURACIÓN DE RUTAS
 # Agregamos la raíz del proyecto al path de Python
